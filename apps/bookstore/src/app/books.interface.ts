1447fe19064f47937df7bb9dc3b7036f16e65cf4
Eliran Eliassy <eliran.eliassy@gmail.com>
2022-11-14 12:30:27 +0200

    test http

---
+++ apps/bookstore/src/app/books.interface.ts
@@ -0,0 +1,5 @@
+export interface Book {
+    title: string;
+    previewImgUrl: string;
+    price?: number;
+}
---
+++ apps/bookstore/src/app/books.service.spec.ts
@@ -0,0 +1,65 @@
+import { BOOKS_MOCK } from './books.mock';
+import { Book } from './books.interface';
+import { TestBed } from '@angular/core/testing';
+
+import { BooksService } from './books.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+
+describe('BooksService', () => {
+  let service: BooksService;
+  let httpController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(BooksService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should getbooks and return value', () => {
+
+    const mock: Book[] = BOOKS_MOCK;
+
+    service.getBooks('Angular')
+      .subscribe((results: Book[]) => {
+
+        expect(results).toBeTruthy();
+        expect(results.length).toBe(10);
+        expect(results[0].title).toEqual(mock[0].title);
+
+      });
+
+    const req = httpController.expectOne(`${service.BASE_URL}?q=Angular`);
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mock);
+
+    httpController.verify();
+
+  });
+
+  it('should test network error', () => {
+    service.getBooks('Angular')
+      .subscribe(
+        () => fail(),
+        (error: HttpErrorResponse) => {
+          expect(error.status).toBe(500);
+          expect(error.message).toEqual('Internal Server Error');
+        }
+      );
+
+    const req = httpController.expectOne(`${service.BASE_URL}?q=Angular`);
+
+    const err = new ErrorEvent('server error', { message: 'Internal Server Error' });
+    req.error(err, { status: 500 });
+
+    httpController.verify();
+  })
+});
