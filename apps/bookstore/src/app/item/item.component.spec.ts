16fd3c230af625768f3f6728acc425299deefe62
Eliran Eliassy <eliran.eliassy@gmail.com>
2022-11-15 11:00:45 +0200

    dubm component

--- apps/bookstore/src/app/item/item.component.spec.ts
+++ apps/bookstore/src/app/item/item.component.spec.ts
@@ -3,6 +3,7 @@ import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemComponent } from './item.component';
+import { Book } from '../books.interface';
 
 describe('ItemComponent', () => {
   let component: ItemComponent;
@@ -15,9 +16,10 @@ describe('ItemComponent', () => {
 
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
+    component.book = BOOKS_MOCK[0];
     fixture.detectChanges();
 
-    component.book = BOOKS_MOCK[0];
+    
   });
 
   it('should create', () => {
@@ -25,9 +27,48 @@ describe('ItemComponent', () => {
   });
 
   it('should render component for spesific item', () => {
-    fixture.detectChanges();
+    
+
 
     const title: HTMLDivElement = fixture.debugElement.query(By.css('.title')).nativeElement;
     expect(title.textContent).toContain(BOOKS_MOCK[0].title);
+
+    const img: HTMLImageElement = fixture.debugElement.query(By.css('img')).nativeElement;
+    expect(img.src).toEqual(BOOKS_MOCK[0].previewImgUrl);
+
+    const price: HTMLDivElement = fixture.debugElement.query(By.css('.price')).nativeElement;
+    expect(Number(price.textContent)).toEqual(BOOKS_MOCK[0].price)
+
+  });
+
+  it('should trigger add to cart', () => {
+    jest.spyOn(component.addToCart, 'emit');
+
+    component.addToCartClicked();
+
+    expect(component.addToCart.emit).toHaveBeenCalledTimes(1);
+
+
+  });
+
+  it('should trigger add to cart - observables', () => {
+    component.addToCart.subscribe((item: Book) => {
+      expect(item).toEqual(BOOKS_MOCK[0]),
+        fail()
+    });
+
+    component.addToCartClicked();
+  });
+
+  it('should remove from cart by clicking the remove btn', () => {
+    jest.spyOn(component.removeFromCart, 'emit');
+    
+
+    const removeBtn = fixture.debugElement.query(By.css('.remove-from-cart-btn')).nativeElement;
+
+    removeBtn.click();
+
+    expect(component.removeFromCart.emit).toHaveBeenCalledTimes(1);
+
   })
 });
